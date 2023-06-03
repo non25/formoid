3681d295c87de037416f01dcccc9b359f41d21a1
kolyamba2105 <haravetskyy@gmail.com>
2023-06-03 16:44:16 +0300

    Adjust Record utils

--- src/utils/Record.ts
+++ src/utils/Record.ts
@@ -1,14 +1,21 @@
 import { Predicate } from "./Predicate";
 
 export function some<T extends Record<string, unknown>>(
-  predicate: Predicate<T[keyof T]>,
   record: T,
+  predicate: Predicate<T[keyof T]>,
 ) {
-  for (const key in record) {
-    if (predicate(record[key])) {
-      return true;
-    }
-  }
+  for (const key in record) if (predicate(record[key])) return true;
 
   return false;
+}
+
+export function mapValues<T extends Record<string, unknown>, B>(
+  record: T,
+  f: (a: T[keyof T], key: keyof T) => B,
+): Record<keyof T, B> {
+  const result = {} as Record<keyof T, B>;
+
+  for (const key in record) result[key] = f(record[key], key);
+
+  return result;
 }
