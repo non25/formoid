f2e5285b887c874ebf778c32f17a76d832bf198d
kolyamba2105 <haravetskyy@gmail.com>
2023-07-28 23:02:36 +0300

    Adjust Array module

--- src/Array.ts
+++ src/Array.ts
@@ -1,22 +1,22 @@
 /**
  * NonEmptyArray
  */
-export interface NonEmptyArray<A> extends Array<A> {
-  0: A;
+export interface NonEmptyArray<T> extends Array<T> {
+  0: T;
 }
 
-export const of = <A>(value: A) => [value] satisfies NonEmptyArray<A>;
+export const of = <T>(value: T): NonEmptyArray<T> => [value];
 
-export function isNonEmpty<A>(as: Array<A>): as is NonEmptyArray<A> {
+export function isNonEmpty<T>(as: Array<T>): as is NonEmptyArray<T> {
   return as.length > 0;
 }
 
-export function isOutOfBound<A>(index: number, as: Array<A>): boolean {
+export function isOutOfBound<T>(index: number, as: Array<T>): boolean {
   return index < 0 || index >= as.length;
 }
 
-export function modifyAt<A>(index: number, fn: (a: A) => A): (as: Array<A>) => Array<A> {
-  return function (as: Array<A>) {
+export function modifyAt<T>(index: number, fn: (a: T) => T): (as: Array<T>) => Array<T> {
+  return function (as: Array<T>) {
     const result = as.slice();
 
     if (isOutOfBound(index, result)) return result;
@@ -27,8 +27,8 @@ export function modifyAt<A>(index: number, fn: (a: A) => A): (as: Array<A>) => A
   };
 }
 
-export function deleteAt<A>(index: number): (as: Array<A>) => Array<A> {
-  return function (as: Array<A>) {
+export function deleteAt<T>(index: number): (as: Array<T>) => Array<T> {
+  return function (as: Array<T>) {
     const result = as.slice();
 
     if (isOutOfBound(index, as)) return result;
