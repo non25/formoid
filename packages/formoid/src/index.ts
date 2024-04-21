7812dc56459c864d119c9350ea1377a7c1a54196
kolyamba2105 <haravetskyy@gmail.com>
2024-04-21 23:11:27 +0300

    Re-export ./Array utils and Validator type

--- packages/formoid/src/index.ts
+++ packages/formoid/src/index.ts
@@ -1,6 +1,8 @@
+export * from "./Array";
 export * from "./Form";
 export * from "./useCompositeForm";
 export * from "./useFieldArray";
 export * from "./useForm";
 export * as validator from "./validator";
 export * from "./zod";
+export { Validator } from "./validator";
