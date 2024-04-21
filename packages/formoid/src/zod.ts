e3b49bccea881e4a2efb5cba132cd790c6666be0
kolyamba2105 <haravetskyy@gmail.com>
2024-04-22 00:48:17 +0300

    Rename Schema -> ZodSchema

--- packages/formoid/src/zod.ts
+++ packages/formoid/src/zod.ts
@@ -6,7 +6,7 @@ import { Validator, failure, success } from "./validator";
 export function fromZod<T, S extends ZodType>(zod: S): Validator<T, TypeOf<S>> {
   return function (value) {
     return zod
-      .safeParseAsync(value)
+      .spa(value)
       .then((result) =>
         result.success
           ? success(result.data)
@@ -15,14 +15,14 @@ export function fromZod<T, S extends ZodType>(zod: S): Validator<T, TypeOf<S>> {
   };
 }
 
-export type Schema<T extends Record<string, unknown>> = {
+export type ZodSchema<T extends Record<string, unknown>> = {
   [K in keyof T]: ZodType | null;
 };
 
-export type ToValidationSchema<T extends Schema<Record<string, unknown>>> = {
+export type ToValidationSchema<T extends ZodSchema<Record<string, unknown>>> = {
   [K in keyof T]: T[K] extends ZodType ? Validator<input<T[K]>, output<T[K]>> : null;
 };
 
-export function fromZodSchema<S extends Schema<Record<string, unknown>>>(schema: S): ToValidationSchema<S> {
+export function fromZodSchema<S extends ZodSchema<Record<string, unknown>>>(schema: S): ToValidationSchema<S> {
   return map(schema, (zod) => (zod === null ? null : fromZod(zod))) as ToValidationSchema<S>;
 }
