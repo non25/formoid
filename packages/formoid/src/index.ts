a87ceac8d4ce453b431b61ae838bf1ee0b7835f5
kolyamba2105 <haravetskyy@gmail.com>
2024-04-22 00:48:04 +0300

    Add more re-exports

--- packages/formoid/src/index.ts
+++ packages/formoid/src/index.ts
@@ -1,8 +1,11 @@
 export * from "./Array";
 export * from "./Form";
+export * from "./Predicate";
+export * from "./Result";
 export * from "./useCompositeForm";
 export * from "./useFieldArray";
 export * from "./useForm";
 export * as validator from "./validator";
 export * from "./zod";
+
 export { Validator } from "./validator";
